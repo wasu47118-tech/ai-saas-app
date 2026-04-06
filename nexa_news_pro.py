141e3eb1252448ab3e6293668d0f5c3bb515b61a
wasu47118-tech <wasu47118@gmail.com>
2026-04-06 22:56:43 +0530

    Update nexa_news_pro.py

--- nexa_news_pro.py
+++ nexa_news_pro.py
@@ -311,7 +311,7 @@ Reply RENEW after payment."""
 # ============================================
 def send_telegram_message(chat_id, message):
     """Send message via Telegram Bot"""
-    if TELEGRAM_BOT_TOKEN == "YOUR_BOT_TOKEN_HERE":
+    if TELEGRAM_BOT_TOKEN == "8756226031:AAFpi0sBKKooqWrQhavpEFyN7peYJs0WfVw":
         return False
     try:
         url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
